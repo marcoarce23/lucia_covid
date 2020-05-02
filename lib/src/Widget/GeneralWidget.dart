9f03eba32683f81209b4525f8b90ec274ea07572
marcoarce23 <marcoarce23@gmail.com>
2020-05-01 23:38:50 -0400

    01.05.2020 23.:38. Clases de logeo, splash y Screen

---
+++ lib/src/Widget/GeneralWidget.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+   Widget copyRigth() {
+     return    Column(
+       crossAxisAlignment :  CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('(imagen) Lucia de cuida. ', style: TextStyle(fontSize: 13.0)),
+            SizedBox(height: 20.0),
+            Text('Derechos Reservados', style: TextStyle(fontSize: 12.0)),
+             Text('CopyRight@LuciaTeCuida.2020', style: TextStyle(fontSize: 12.0)),
+          ],
+        );
+        
+  }
--- lib/src/module/SplashScreen/SplashScreenModule.dart
+++ lib/src/module/SplashScreen/SplashScreenModule.dart
@@ -1,37 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:lucia_covid/src/module/Login/SignUpModule.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'package:lucia_covid/src/module/InitialPages/SlideShowModule.dart';
-
 
 class SplashScreenModule extends StatefulWidget {
   @override
   SplashScreenModuleState createState() => SplashScreenModuleState();
 }
 
-class SplashScreenModuleState extends State<SplashScreenModule> {  
-
+class SplashScreenModuleState extends State<SplashScreenModule> {
   @override
   Widget build(BuildContext context) {
-    return  SplashScreen( seconds: 5,
-      navigateAfterSeconds: new SliderShowModule(),
-      title: new Text('Prevencion COVID-19', style:TextStyle( fontSize: 25, color: Colors.white )  ,),
-      image:  Image(
-            image: AssetImage('assets/image/COVID-19.png'),
-            width: MediaQuery.of(context).size.width / 2,
-            fit: BoxFit.contain,
-          ),
+    return SplashScreen(
+      seconds: 5,
+      navigateAfterSeconds: new SignUpModule(),
+      title: new Text(
+        'Prevencion COVID-19',
+        style: TextStyle(fontSize: 25, color: Colors.white),
+      ),
+      image: Image(
+        image: AssetImage('assets/image/COVID-19.png'),
+        width: MediaQuery.of(context).size.width / 2,
+        fit: BoxFit.contain,
+      ),
       backgroundColor: Colors.black,
-      
       photoSize: 100.0,
-      loaderColor: Colors.white);
-    /*return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _ImagenLogo(),
+      gradientBackground: RadialGradient(
+        colors: [
+          Color(0xFF3594DD),
+          Color(0xFF4563DB),
+          Color(0xFF5036D5),
+          Color(0xFF5B16D0),
         ],
       ),
     );
-    */
   }
-     
 }
+//RadialGradient
