5cba0fdbf55202030da13af233d3a5ff5712bad3
Christian Alba <coavchristian@hotmail.com>
2020-05-24 00:03:50 -0400

    24/05/2020 12:03 intro image

--- lib/src/module/SplashScreen/SplashScreenModule.dart
+++ lib/src/module/SplashScreen/SplashScreenModule.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucia_covid/src/Util/Resource.dart';
 import 'package:lucia_covid/src/module/Login/SignUpModule.dart';
 import 'package:splashscreen/splashscreen.dart';
 
@@ -11,27 +12,16 @@ class SplashScreenModuleState extends State<SplashScreenModule> {
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
-      seconds: 5,
+      seconds: 7,
       navigateAfterSeconds: new SignUpModule(),
-      title: new Text(
-        'LUCIA TE CUIDA',
-        style: TextStyle(fontSize: 25, color: Colors.white),
-      ),
+     
+      photoSize: 250,
       image: Image(
-        image: AssetImage('assets/image/COVID-19.png'),
-        width: MediaQuery.of(context).size.width / 2,
-        fit: BoxFit.contain,
+        image: AssetImage('assets/fondo.png') ,       
+        fit: BoxFit.cover,
       ),
-      backgroundColor: Colors.black,
-      photoSize: 100.0,
-      gradientBackground: LinearGradient(
-        colors: [
-          Color.fromRGBO(215, 78, 159, 1.0),
-                    Color.fromRGBO(245, 173, 53, 1.0),
-                    Color.fromRGBO(236, 220, 109, 1.0),
-                    Color.fromRGBO(70, 191, 167 , 1.0),
-        ],
-      ),
+    
+  
     );
   }
 }
